804c919dea0008cabf4c6efcf6e2669822320075
MathieuProal <mathieu.proal@gmail.com>
2025-12-24 17:11:24 +0000

    td3

---
+++ devops_base/td3/scripts/packer/app.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+apps : [{
+name : "sample-app",
+script : "./app.js",
+exec_mode : "cluster",
+instances : "max",
+env: {
+"NODE_ENV": "production"
+}
+}]
+}
+
---
+++ devops_base/td3/scripts/tofu/live/asg-sample/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+region = "us-east-2"
+}
+module "asg" {
+source = "github.com/Samy-Meliani/Devops-Project/devops_base/td3/scripts/tofu/modules/asg"
+name = "sample-app-asg"
+ami_id = "256177"
+user_data = filebase64("${path.module}/user-data.sh")
+app_http_port = 8080
+instance_type = "t2.micro"
+min_size = 1
+max_size = 10
+desired_capacity = 3
+}
+
