929f01ebd51b4afb77cfd682bf67386b1b3f84e4
Mathieu PROAL <mathieu.proal@gmail.com>
2026-01-01 17:42:13 +0100

    Introduce intentional error

--- devops_base/td5/scripts/sample-app/app.js
+++ devops_base/td5/scripts/sample-app/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('DevOps Labs!');
 });
 app.get('/name/:name', (req, res) => {
     res.send(`Hello, ${req.params.name}!`);
